044da030d2e369f84c444829db3a33f2524dd55d
Asianadian <danielchen.mc@gmail.com>
2024-09-28 16:23:08 -0400

    Began mergning client files

--- source/betterClient.py
+++ source/betterClient.py
@@ -3,18 +3,18 @@ import threading
 from threading import Lock
 from structures import FileByteStream
 
-networkFileData =[]
-networkFileDataLock = Lock()
+networkFiles = {}
+networkFilesLock = Lock()
 
 def main():
     #TODO start daniels thread code
     while True:
-
-        networkFileDataLock.acquire()
+        global networkFilesLock
+        networkFilesLock.acquire()
         print("These are the availible files: ")
-        for file in networkFileData:
-            print(FileByteStream(file).name)
-        networkFileDataLock.release()
+        for key, value in networkFiles.tems():
+            print(FileByteStream(key).name)
+        networkFilesLock.release()
         
         fileName = input("Which file would you like to download? or type q to quit: ")
         if (fileName == 'q'):
